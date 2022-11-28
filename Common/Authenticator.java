a2710585feec4d5949da69ce47bb9ac0f827b8e8
Fern-Tree <112579562+Fern-Tree@users.noreply.github.com>
2022-11-28 13:43:20 -0700

    New Authenticator

--- Common/Authenticator.java
+++ Common/Authenticator.java
@@ -8,7 +8,6 @@ public class Authenticator {
 	public static void main (String[] args) throws IOException {
 		//Creating Variables
 		boolean user = false;
-		boolean password = false;
 		String userName = null;
 		String userPass = null;
 		
@@ -16,91 +15,40 @@ public class Authenticator {
 		@SuppressWarnings("resource")
 		Scanner in = new Scanner(System.in);
 		//Asking the user to input user name
-		System.out.println("Hello User! Please enter your Username!");
-			//Do While to put in user names until one is correct
+		System.out.println("Hello User!");
 			do {
-			userName = in.next();
-			
-			//If user wishes to exit
-			if(userName.toLowerCase().equals("exit")) {
-				System.out.println("User has chosen to exit. Goodbye!");
-				System.exit(0);
-			}
-			
-			//Calling the user names checker
-			user = fileUsernames(userName);
-			//If user name is correct
-			if(user == true) {
-				System.out.println("We have found your Username!");
-			}
-			//If user name is incorrect
-			else {
-				System.out.println("We did not find your Username! Please try again!");
-			}
-		//Repeats until correct user name input
-		} while(user == false);
-		
-		//Asks user to input their password
-		System.out.println("Now we just need your password!");
-		userPass = in.next();
-		
-		//Calling the password checker
-		password = filePasswords(userPass);
-		
-		//If password is correct
-		if(password == true) {
-			System.out.println("Hello " + userName + "! Welcome to the chicken calculator!");
-		}
-		//If password is incorrect
-		else {
-			System.out.println("Sorry! That is the wrong password! Goodbye!");
-			//Exits the system
-			System.exit(0);
-		}
+				//Gets the user name and password from the user
+				System.out.println("Please enter your user name!");
+				userName = in.nextLine();
+				System.out.println("Please enter your password!");
+				userPass = in.nextLine();
+				user = users(userName, userPass);
+				
+				if(user == true) {
+					System.out.println("Welcome " + userName + "!");
+				}
+				else {
+					System.out.println("Authentication failed, please try again.");
+				}
+			} while (user == false);
 	}
 
-	public static Boolean fileUsernames (String inputedUser) throws IOException {
+	public static Boolean users (String inputedUser, String inputedPass) throws IOException {
 		//Creating Variables
-		boolean userFound = false;
-		String fileUsernames = null;
+		boolean found = false;
+		String fileUser = null;
 		
 		//Finds the file
-		File usernames = new File ("C:\\Users\\bluej\\eclipse-workspace\\Autentication\\src\\checkUsername\\Usernames.txt");
-		//Creating file reader
-		Scanner fileReader = new Scanner(usernames);
-		
-		//Goes through the file
-		while(fileReader.hasNextLine()) {
-			fileUsernames = fileReader.nextLine();
-			//Tests each line of the file against inputed user name
-			if(fileUsernames.equals(fileUsernames)) {
-				userFound = true;
-			}
-		}
-		
-		//Closes fileReader
-		fileReader.close();
-		
-		//Returns if the user name was found or not
-		return userFound;
-	}
-	
-	public static Boolean filePasswords (String inputedPass) throws IOException {
-		//Creating Variables
-		boolean correctPass = false;
-		String filePass = null;
-		
-		//Finds the file
-		File passwords = new File ("C:\\Users\\bluej\\eclipse-workspace\\Autentication\\src\\checkUsername\\Passwords.txt");
+		File passwords = new File ("C:\\Users\\bluej\\eclipse-workspace\\Autentication\\src\\checkUsername\\Users.txt");
 		//Creating file reader
 		Scanner fileReader = new Scanner(passwords);
 		
 		//Goes through the file
 		while(fileReader.hasNextLine()) {
-			filePass = fileReader.nextLine();
+			fileUser = fileReader.nextLine();
 			//Tests each line of the file against inputed password
-			if(inputedPass.equals(filePass)) {
-				correctPass = true;
+			if(fileUser.contains(inputedUser) && fileUser.contains(inputedPass)) {
+				found = true;
 			}
 		}
 		
@@ -108,6 +56,6 @@ public class Authenticator {
 		fileReader.close();
 		
 		//Returns if the password was found or not
-		return correctPass;
+		return found;
 	}
 }
