2da69962ecbd400be0e19b2529b8a9d4e96f9248
= <=>
2022-11-30 12:31:16 -0700

    Updated to dynamically detect OS family for cross-platform

--- Common/Driver.java
+++ Common/Driver.java
@@ -5,6 +5,11 @@ import GUI.MainGUI;
 public class Driver {
     
     public static void main(String[] args) {
+
+        // Set the OS family correctly
+        AdjustOS.detectOS();
+
+        // Start the MainGUI family of actions
         MainGUI.main(args);
     }
 
